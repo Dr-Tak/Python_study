c8edc9001100a4cd9d7c5bd6791ab4b5084f504b
Dr-Tak <45122833+Dr-Tak@users.noreply.github.com>
2019-02-17 01:16:38 +0900

    Create itunes_getter.py

---
+++ itunes_getter.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from logging import getLogger, DEBUG
+import urllib.request
+import json
+import binascii
+
+# ログの設定
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+
+# 定数です
+NUM_SPHERE = 4
+NUM_TRYSAIL = 3
+
+
+def main():
+    a = 'test'
+    logger.debug("mainの開始")
+    
+    if a != 'test':
+        func_fugafuga(1)
+    else:
+        print(a)
+        
+    #ここからちゃんと書くべ
+    #まずはitunesにGETリクエストを投げるよ
+    #url 初期化
+    url = ''
+    
+    #urlは構造で分解する必要がある
+    protocol = 'https://'
+    domain = 'itunes.apple.com/search?term='
+    word = 'Mr\.children'#検索キーワードさ
+    query_string = '&media=music'
+    
+    url = protocol + domain + word + query_string
+    print(url)
+    
+#     req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+    
+    print(type(body))
+    print(body)
+    
+    
+    #JSONの解析
+    #musicのdict
+    
+def func_fugafuga(parm1):
+    logger.error("エラーです！！")
+
+class MyClass:
+
+    def __init__(self):
+        self._pv_v = "インスタンス変数"
+
+    def process(self, parm1):
+        logger.debug("process")
+
+    def _pv_process(self):
+        logger.debug("_pv_process")
+
+
+if __name__ == "__main__":
+    main()
