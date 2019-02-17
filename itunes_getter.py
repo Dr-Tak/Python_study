8009688f9b8e7060d9dac78d52629a3811abb35f
Dr-Tak <45122833+Dr-Tak@users.noreply.github.com>
2019-02-17 22:12:54 +0900

    itunesの値を解析する事はできた　一旦簡単なコーディングを完とする
    詳細のチェックなんかは後追い

--- itunes_getter.py
+++ itunes_getter.py
@@ -8,51 +8,38 @@ import binascii
 logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 
-# 定数です
-NUM_SPHERE = 4
-NUM_TRYSAIL = 3
-
-
 def main():
-    a = 'test'
     logger.debug("mainの開始")
     
-    if a != 'test':
-        func_fugafuga(1)
-    else:
-        print(a)
-        
-    #ここからちゃんと書くべ
-    #まずはitunesにGETリクエストを投げるよ
     #url 初期化
     url = ''
     
     #urlは構造で分解する必要がある
     protocol = 'https://'
     domain = 'itunes.apple.com/search?term='
-    word = 'Mr\.children'#検索キーワードさ
+    word = 'Mr\.children'#検索キーワード　HTMLから値をもらってくる箇所(いずれ変数化する)
     query_string = '&media=music'
     
     url = protocol + domain + word + query_string
     print(url)
     
-#     req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         json_txt = json.load(res)  #JSON形式での取得
 
-#     print(body)
-    
     #JSONの解析
     #musicのdict
-    keyList = json_txt.keys()
-#     print(keyList)
+    for one in json_txt['results']:  #普通に配列へのアクセスで取れたわ・・・
+        print(
+            one['artistName'],
+            one['collectionName'],
+            one['trackName'],
+            one['artworkUrl100'],
+            one['trackNumber'],
+            one['country']
+        )
     
-    valList = json_txt.values()
-    print(valList)
-    
-    
-def func_fugafuga(parm1):
+def func_erroe(parm1):
     logger.error("エラーです！！")
 
 class MyClass:
